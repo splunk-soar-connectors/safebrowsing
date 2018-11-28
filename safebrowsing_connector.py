43505b52e448567815594e36bbf05ad7034bab84
farhanbhoraniya_crest <farhan.bhoraniya@crestdatasys.com>
2018-11-28 15:32:20 +0530

    Removed redundant code from the end

--- safebrowsing_connector.py
+++ safebrowsing_connector.py
@@ -163,9 +163,3 @@ if __name__ == '__main__':
         print (json.dumps(json.loads(ret_val), indent=4))
 
     exit(0)
-)
-        connector.print_progress_message = True
-        ret_val = connector._handle_action(json.dumps(in_json), None)
-        print (json.dumps(json.loads(ret_val), indent=4))
-
-    exit(0)
